5b49333b4ee20b431d73dcf51dc3b7e177c997aa
Mederpomeder <mederpomeder@gmail.com>
2023-01-31 21:24:11 +0600

    added orders

--- product/views.py
+++ product/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import permissions, response
 from rest_framework.decorators import action
+
+from rating.serializers import ReviewSerializer, ReviewActionSerializer
 from .models import Product
 from . import serializers
 from .permissions import IsAuthor
@@ -23,3 +25,28 @@ class ProductViewSet(ModelViewSet):
             return [permissions.IsAuthenticated(), IsAuthor()]
         return [permissions.IsAuthenticatedOrReadOnly()]
 
+    @action(['GET', 'POST'], detail=True)
+    def reviews(self, request, pk):
+        product = self.get_object()
+        if request.method == 'GET':
+            reviews = product.reviews.all()
+            serializer = ReviewActionSerializer(reviews, many=True).data
+            return response.Response(serializer, status=200)
+        else:
+            if product.reviews.filter(owner=request.user).exists():
+                return response.Response('You\'ve already reviewed this product!', status=400)
+            data = request.data
+            serializer = ReviewActionSerializer(data=data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save(owner=request.user, product=product)
+            return response.Response(serializer.data, status=201)
+
+    @action(['DELETE'], detail=True)
+    def review_delete(self, request, pk):
+        product = self.get_object()
+        user = request.user
+        if not product.reviews.filter(owner=user).exists():
+            return response.Response('You didn\'t reviewed this product!', status=400)
+        review = product.reviews.get(owner=user)
+        review.delete()
+        return response.Response('Successfully deleted!', status=204)
