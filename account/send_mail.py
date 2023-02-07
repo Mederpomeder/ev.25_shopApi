8072e7d6b64ef50fe5bb80a56c7797886571b63b
Mederpomeder <mederpomeder@gmail.com>
2023-02-07 18:53:32 +0600

    added server link

--- account/send_mail.py
+++ account/send_mail.py
@@ -3,9 +3,10 @@ from django.core.mail import send_mail
 
 def send_confirmation_email(user, code):
     full_link = f'http://localhost:8000/api/v1/accounts/activate/{code}/'
+    full_link_server = f'http://34.67.83.101/api/v1/accounts/activate/{code}/'
     send_mail(
         'Здравствуйте активируйте свой аккаунт!',
-        f'Чтобы активировать нужно перейти по ссылке: \n{full_link}',
+        f'Чтобы активировать нужно перейти по ссылке: \n{full_link}\n{full_link_server}',
         'mederpomeder@gmail.com',
         [user],
         fail_silently=False,
